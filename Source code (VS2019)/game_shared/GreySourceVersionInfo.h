f7a9b1467ef222d1833b76fba437720a9f32a14e
sabianroberts <djdise@outlook.com>
2020-11-26 21:46:51 +0000

    Correct info

--- Source code (VS2019)/game_shared/GreySourceVersionInfo.h
+++ Source code (VS2019)/game_shared/GreySourceVersionInfo.h
@@ -4,6 +4,6 @@
 #define buildType "Propietary" // There are two types of Grey Source builds — Basic, meaning the engine and SDK is unmodified, and Propietary, meaning the engine and/or SDK has been modified for the specific game.
 #define buildDate __DATE__
 #define buildAuthor "Grey Source 2020 by The Phoenix Project"
-#define build "Grey Source 2020 Propietary [build 2005-Z]"
+#define build "Grey Source 2020 Proprietary [build 2011-Z]"
 #define gameSdk "https://github.com/phoenixprojectsoftware/zamnhl-sdk"
 #define engineSdk "https://github.com/phoenixprojectsoftware/greysource"
